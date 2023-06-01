0b15fcfdb2cb06ac5f160ddc1affe99568d30156
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-05-31 20:35:47 -0700

    implemented get_timestamp()

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,5 @@
 use std::sync::{Arc, Mutex};
-use tonic::Request;
+use tonic::{Request, Response};
 
 use crate::error::{Result, RpcResult};
 use crate::proto::placement_driver::{PlacementDriver, TsoRequest, TsoReply, DataLocRequest, DataLocReply};
@@ -13,23 +13,17 @@ pub struct FeatherPD {
 impl FeatherPD {
     /// Creates a new FeatherPD server.
     pub fn new() -> Result<Self> {
-        Ok(Self {
-            next_ts: Arc::new(Mutex::new(0)),
-        })
-    }
-
-    /// Gets the next timestamp.
-    pub fn get_next_ts(&self) -> u64 {
-        let mut next_ts = self.next_ts.lock().unwrap();
-        *next_ts += 1;
-        *next_ts
+        Ok(Self { next_ts: Arc::new(Mutex::new(0)) })
     }
 }
 
 #[tonic::async_trait]
 impl PlacementDriver for FeatherPD {
     async fn get_timestamp(&self, _request: Request<TsoRequest>) -> RpcResult<TsoReply> {
-        todo!()
+        let mut next_ts = self.next_ts.lock().unwrap();
+        let reply = TsoReply { timestamp: *next_ts };
+        *next_ts += 1;
+        Ok(Response::new(reply))
     }
 
     async fn get_data_location(&self, _request: Request<DataLocRequest>) -> RpcResult<DataLocReply> {
