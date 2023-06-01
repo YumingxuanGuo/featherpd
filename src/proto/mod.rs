eeb7d7098e694923a681e4eb1c00de6198509d60
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-05-31 20:31:08 -0700

    pub used RPC related modules

--- src/proto/mod.rs
+++ src/proto/mod.rs
@@ -1,3 +1,5 @@
 pub mod placement_driver {
     tonic::include_proto!("placement_driver");
+    pub use placement_driver_server::{PlacementDriver, PlacementDriverServer};
+    pub use placement_driver_client::PlacementDriverClient;
 }
